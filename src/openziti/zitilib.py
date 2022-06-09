367a1ad3e28a98bf75a3eea68ec00418ce7c8160
Eugene K <eugene.kobyakov@netfoundry.io>
2022-06-09 16:42:18 -0400

    implement hosting functions raise exceptions on errors

--- src/openziti/zitilib.py
+++ src/openziti/zitilib.py
@@ -43,6 +43,8 @@ class _Ver(ctypes.Structure):
 _ziti_version = ziti.ziti_get_version
 _ziti_version.restype = ctypes.POINTER(_Ver)
 
+_ziti_lasterr = ziti.Ziti_last_error
+
 _ziti_errorstr = ziti.ziti_errorstr
 _ziti_errorstr.argtypes = [ctypes.c_int]
 _ziti_errorstr.restype = ctypes.c_char_p
@@ -55,14 +57,21 @@ ziti_socket = ziti.Ziti_socket
 ziti_socket.argtypes = [ctypes.c_int]
 ziti_socket.restype = ctypes.c_int
 
-ziti_connect = ziti.Ziti_connect
-ziti_connect.argtypes = [ctypes.c_int, ctypes.c_void_p, ctypes.c_char_p]
-
+_ziti_connect = ziti.Ziti_connect
+_ziti_connect.argtypes = [ctypes.c_int, ctypes.c_void_p, ctypes.c_char_p]
 
 _ziti_connect_addr = ziti.Ziti_connect_addr
 _ziti_connect_addr.argtypes = [ctypes.c_int, ctypes.c_char_p, ctypes.c_int]
 _ziti_connect_addr.restype = ctypes.c_int
 
+_ziti_bind = ziti.Ziti_bind
+_ziti_bind.argtypes = [ctypes.c_int, ctypes.c_void_p, ctypes.c_char_p]
+
+_ziti_listen = ziti.Ziti_listen
+_ziti_listen.argtypes = [ctypes.c_int, ctypes.c_int]
+
+_ziti_accept = ziti.Ziti_accept
+_ziti_accept.argtypes = [ctypes.c_int, ctypes.c_char_p, ctypes.c_int]
 
 _ziti_enroll = ziti.Ziti_enroll_identity
 _ziti_enroll.argtypes = [
@@ -88,6 +97,16 @@ def errorstr(code):
     return msg.decode()
 
 
+def check_error(code):
+    if code != 0:
+        err = _ziti_lasterr()
+        if err < 0:
+            msg = _ziti_errorstr(err).decode(encoding='utf-8')
+        else:
+            msg = errorstr(err)
+        raise Exception(err, msg)
+
+
 def init():
     ziti.Ziti_lib_init()
 
@@ -101,11 +120,33 @@ def load(path):
     return _load_ctx(b_obj)
 
 
-def connect(fd, addr: Tuple[str, int]):
+def connect(fd, ztx, service: str):
+    srv = bytes(service, encoding='utf-8')
+    check_error(_ziti_connect(fd, ztx, srv))
+
+
+def connect_addr(fd, addr: Tuple[str, int]):
     # pylint: disable=invalid-name
     host = bytes(addr[0], encoding='utf-8')
     port = addr[1]
-    return _ziti_connect_addr(fd, host, port)
+    check_error(_ziti_connect_addr(fd, host, port))
+
+
+def bind(fd, ztx, service):
+    srv = bytes(service, encoding='utf-8')
+    check_error(_ziti_bind(fd, ztx, srv))
+
+
+def listen(fd, backlog):
+    check_error(_ziti_listen(fd, backlog))
+
+
+def accept(fd):
+    b = ctypes.create_string_buffer(128)
+    clt = _ziti_accept(fd, b, 128)
+    if clt < 0:
+        check_error(clt)
+    return clt,(bytes(b.value).decode('utf-8'), 0)
 
 
 def enroll(jwt, key=None, cert=None):
