7081a205475445bdadcc356ef04860a7e483fa64
Eugene K <eugene.kobyakov@netfoundry.io>
2022-08-31 14:12:41 -0400

    initial doc

--- src/openziti/context.py
+++ src/openziti/context.py
@@ -19,15 +19,26 @@ from . import zitilib, zitisock
 
 
 class ZitiContext:
+    """
+    Object representing a Ziti Identity.
+    """
     def __init__(self, ctx):
         if not isinstance(ctx, int):
             raise TypeError("ctx is not a valid python void pointer type")
         self._ctx = ctx
 
-    def connect(self, addr):
+    def connect(self, addr, terminator=None):
+        """
+        Connect to a Ziti service.
+        Either service name or intercept address(host,port) could be used to connect to service.
+
+        :param addr: service name (string) or service intercept address(tuple[host, port])
+        :param terminator: specific terminator for the given service, ignored is [addr] is an intercept address
+        :return: socket connected to the service
+        """
         fd = zitilib.ziti_socket(socket.SOCK_STREAM)
         if isinstance(addr, str):
-            zitilib.connect(fd, self._ctx, addr)
+            zitilib.connect(fd, self._ctx, service=addr, terminator=terminator)
         elif isinstance(addr, tuple):
             zitilib.connect_addr(fd, addr)
         else:
@@ -35,6 +46,14 @@ class ZitiContext:
         return socket.socket(socket.AF_UNIX, socket.SOCK_STREAM, 0, fd)
 
     def bind(self, service, terminator=None, sock=None):
+        """
+        Bind to a Ziti service and return server socket.
+
+        :param service: service name to bind to
+        :param terminator: (optional) bind with specific terminator
+        :param sock: existing socket to bind
+        :return: server socket ready for accept() call
+        """
         if sock is None:
             sock = zitisock.ZitiSocket(type=socket.SOCK_STREAM)
         zitilib.bind(sock.fileno(), self._ctx, service, terminator)
@@ -42,6 +61,12 @@ class ZitiContext:
 
     @classmethod
     def from_path(cls, path):
+        """
+        Load Ziti Identity
+
+        :param path: path to Ziti Identity file
+        :return: ZitiContext representing given identity
+        """
         if not isinstance(path, str):
             raise TypeError("path must be a string")
         if not (isfile(path) or isdir(path)):
@@ -50,6 +75,12 @@ class ZitiContext:
 
 
 def load_identity(path) -> ZitiContext:
+    """
+    Load Ziti Identity
+
+    :param path: path to Ziti Identity file
+    :return: Ziti Context object representing Ziti Identity
+    """
     return ZitiContext.from_path(path)
 
 
